f29e373c99ec43c924a50e1523c25f3cb36e45fb
deenoc <deenoo.g@gmail.com>
2025-05-26 13:13:10 -0400

    ✅ Final Sprint 1: Fixed indentation, 26 passing tests, 96% coverage

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -194,12 +194,13 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-######################################################################
-# TEST AN ACCOUNT NOT FOUND
-######################################################################
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
