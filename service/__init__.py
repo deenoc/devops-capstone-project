0a58c1e12841f098aeda2a89428560d7865b6551
deenoc <deenoo.g@gmail.com>
2025-05-27 06:50:40 -0400

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -35,3 +35,7 @@ except Exception as error:  # pylint: disable=broad-except
     sys.exit(4)
 
 app.logger.info("Service initialized!")
+
+from flask_talisman import Talisman
+
+talisman = Talisman(app)
